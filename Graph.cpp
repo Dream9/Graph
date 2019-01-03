550f0994e955760c4280c29450acc3b40539aedc
Dream9 <45119286+Dream9@users.noreply.github.com>
2019-01-03 20:06:40 +0800

    Add files via upload

--- Graph.cpp
+++ Graph.cpp
@@ -14,92 +14,16 @@
 #include<fstream>
 #include<iostream>
 
-inline Graph::Graph(int num) {
-	if (num < 0)
-		throw Graph_err(std::to_string(__LINE__)+__FILE__+_v_err);
-	this->v = num;
-	this->e = 0;
-	this->arr.resize(v, {});
-}
-Graph::Graph(std::istream& in) {
-	int v;
-	int e;
-	int w;
 
-	in >> v >> e;
-	this->v = v;
-	this->e = 0;
-	if(v<0)
-		throw Graph_err(std::to_string(__LINE__) + __FILE__ +_v_err);
-	if (e < 0)
-		throw Graph_err(std::to_string(__LINE__) + __FILE__ + _e_err);
-	this->arr.resize(v, {});
-	while (e-->0) {
-		in >> v >> w;
-		validate(v);
-		validate(w);
-		addEdge(v, w);
-	}
-
-}
-
-void Graph::addEdge(int first, int second) {
-	//注意这里没有考虑加点的情况
-	--first;//注意点序列为123
-	--second;//对应索引为012
-	arr[first].push_back(second);
-	arr[second].push_back(first);
-	++e;
-}
-
-const Graph::Iterator& Graph::adj(int num) const {
-	//返回相连顶点的vector的引用
-	//这里采用vector，，有点浪费空间
-	--num;
-	this->validate(num);
-	return this->arr[num];
-}
-
-std::string Graph::toString()
-{
-	//转换为字符串输出
-	int iter = 0;
-	std::string out = "";
-
-	out += std::to_string(this->v) + "vertecies" + std::to_string(this->e) + "edge\r\n";
-	for (; iter < this->v; ++iter) {
-		out += std::to_string(iter) + ":";
-		for (int jter : this->adj(iter)) {
-			out += ","+std::to_string(jter);
-		}
-		out += "\r\n";
-	}
-	return out;
-}
-
-std::vector<int> Graph::DepthFirstSearch(int num) {
-	//接口，调用dfs友元
-	int len = this->V();
-	std::vector<int> out;
-	std::vector<bool>visited(len,false);
-	//out.reserve(len);
-	dfs(*this, num, visited,out);
-	return out;
-}
-void dfs(const Graph& G, int num, std::vector<bool>&visited, std::vector<int>&out) {
-	visited[num]=true;
-	for (int cur : G.adj(num)) {
-		if (!visited[cur])
-			dfs(G, cur, visited, out);
-	}
-}
 
 
 /*测试函数*/
 int main(int argc, char**argv) {
 	std::ifstream ceshi;
 	ceshi.open("F:/yzhh/VS2017pro/Graph/Graph/data.txt");
-	Graph test(ceshi);
+	Graph<int> test(ceshi);
+	std::vector<int>dfs_ans = test.BreathFirstSearch(1);
 	std::cout << test.V();
+	
 	ceshi.close();
 }
--- Graph.h
+++ Graph.h
@@ -7,6 +7,17 @@
 #include<vector>
 #include<string>
 #include<exception>
+#include<map>
+#include<set>
+#include<queue>
+
+const std::string _v_err = "顶点数必须非负";
+const std::string _e_err = "边数目必须非负";
+template<typename> class Graph;
+template<typename T>
+void bfs(const Graph<T>&,T,std::vector<T>&);
+template<typename T>
+void dfs(const Graph<T>&, T, std::set<T>, std::vector<T>&);
 
 //定义错误抛出类型
 class Graph_err :public std::runtime_error{
@@ -37,42 +48,55 @@ struct Node_weight :public Node<T> {
 //定义edge
 template<typename T>
 struct Edge {
-	T key;
+	T to;
+	Edge(T val) :to(val) {};
 };
 //
 template<typename T,typename W>
 struct weightEdge :public Edge<T> {
+	T from;
 	W weight;
+	weightEdge(T _f, T _t) :from(_f), to(_t) {};
+	bool operator<(const weightEdge&a) { return std::less<W>(this->weight, a.weight) };
 };
 
 //定义Graph
+template<typename T>
 class Graph {
 
 public:
-	typedef std::vector<int> Iterator;
-
+	typedef Edge<T> entity;
+	typedef std::vector<entity> Iterator;
+	
 private:
+
 	int v;//顶点数目
 	int e;//边的数目
-	std::vector<std::vector<int>>arr;//邻接表数组
+	std::map<T,Iterator>arr;//邻接表数组,,考虑了采用键值对,,也可以采用 vector
 
 public:
 	Graph(int num);//创建一个含有num个顶点但不含边的图
 	Graph(std::istream& in);//从标准输入流中读入一幅图
-	void addEdge(int first, int second);//向图中添加一条边
-	const Iterator& adj(int num) const;//和v相邻的顶点,只是遍历，不能做修改
+	void addEdge(T first, T second);//向图中添加一条边
+	const Iterator& adj(T num) const;//和v相邻的顶点,只是遍历，不能做修改
 	std::string toString();//Graph的字符串表达
 	/*遍历*/
-	std::vector<int> DepthFirstSearch(int num);//图，起始点
-	friend void dfs(const Graph& G, int num, std::vector<bool>&visited, std::vector<int>&out);
-
+	//dfs版
+	std::vector<T> DepthFirstSearch(T num);//图，起始点
+	friend void dfs<T>(const Graph<T> &, T, std::set<T>&, std::vector<T>&);
+	//bfs版
+	std::vector<T> BreathFirstSearch(T num);
+	friend void bfs<T> (const Graph& G, T num, std::vector<T>&out);
 
 public:
 	int V() const { return v; }//返回点数
 	int E() const { return e; }//返回边数
 	virtual void validate(int arg) const {
+		//主要保证在使用vector时候不会越界
+		/*
 		if (arg<0 || arg>v)
 			throw Graph_err("Invalid argument");
+		*/
 	}//有效性检验
 	size_t degre(int num) {
 		validate(num);
@@ -80,17 +104,141 @@ public:
 	}//某个点的度数
 
 private:
-	void 
+	//
 };
 
 
 
-const std::string _v_err = "顶点数必须非负";
-const std::string _e_err = "边数目必须非负";
 
 
+/*具体实现*/
+template<typename T>
+inline Graph<T>::Graph(int num) {
+	if (num < 0)
+		throw Graph_err(std::to_string(__LINE__) + __FILE__ + _v_err);
+	this->v = num;
+	this->e = 0;
+	this->arr.resize(v, {});
+}
+template<typename T>
+Graph<T>::Graph(std::istream& in) {
+	T first;
+	T second;
+	int iter;
+	in >> this->v >> iter;
 
+	if (this->v < 0)
+		throw Graph_err(std::to_string(__LINE__) + __FILE__ + _v_err);
+	if (this->e < 0)
+		throw Graph_err(std::to_string(__LINE__) + __FILE__ + _e_err);
+	//如果不采用vector,下面的可以忽略
+	//this->arr.resize(v, {});
+	while (iter-- > 0) {
+		in >> first >> second;
+		validate(first);
+		validate(second);
+		addEdge(first, second);
+	}
 
+}
+
+/*
+ *Graph的基本实现
+ **  A graph, implemented using an array of sets.
+ *  Parallel edges and self-loops allowed.
+ *
+ *   a Graph tinyG.txt 期待以下结构
+ * 13 13    //代表v,e
+ * 0 6      //代表边
+ * 1 2      //...
+ *
+ */
+template<typename T>
+void Graph<T>::addEdge(T first, T second) {
+	//重新采用map规划后，不需下面两句
+	//--first;//注意点序列为123
+	//--second;//对应索引为012
+	arr[first].push_back(entity(second));
+	arr[second].push_back(entity(first));
+	++e;
+}
+template<typename T>
+const typename Graph<T>::Iterator& Graph<T>::adj(T num) const {
+	//返回相连顶点的vector的引用
+	//--num;
+	//this->validate(num);
+	auto ans = this->arr.find(num);
+	if (ans == this->arr.end())
+		throw;
+	return ans->second;
+}
+template<typename T>
+std::string Graph<T>::toString()
+{
+	//转换为字符串输出
+	int iter = 0;
+	std::string out = "";
+
+	out += std::to_string(this->v) + "vertecies" + std::to_string(this->e) + "edge\r\n";
+	for (; iter < this->v; ++iter) {
+		out += std::to_string(iter) + ":";
+		for (entity jter : this->adj(iter)) {
+			out += "," + std::to_string(jter.to);
+		}
+		out += "\r\n";
+	}
+	return out;
+}
+
+template<typename T>
+std::vector<T> Graph<T>::DepthFirstSearch(T num) {
+	//接口，调用dfs友元
+	int len = this->V();
+	std::vector<T> out;
+	std::set<T>visited;
+	//out.reserve(len);
+	dfs(*this, num, visited, out);
+	return out;
+}
+
+template<typename T>
+std::vector<T> Graph<T>::BreathFirstSearch(T num){
+	//接口，调用bfs
+	std::vector<T> out;
+	bfs(*this, num, out);
+	return out;
+
+}
+/*友元bfs的定义*/
+template<typename T>
+void bfs(const Graph<T>& G, T num, std::vector<T>&out) { 
+	/*借助于queue实现*/
+	std::queue<T>que;
+	que.push(num);
+	std::set<T>visited;
+	while (!que.empty()) {
+		T current = que.front();
+		que.pop();
+		if (visited.insert(current).second) {
+			for (auto iter : G.adj(current)) {
+				que.push(iter.to);
+			}
+			out.push_back(current);
+		}
+	}
+
+}/*友元dfs的定义*/
+template<typename T>
+void dfs(const Graph<T> & G, T num, std::set<T>&visited, std::vector<T>&out) {
+		/*由于采用了map的实现，那么也就不能用vector记录访问了，改用set*/
+	/*借助stack，也可以不用递归*/
+	out.push_back(num);
+	visited.insert(num);
+	for (typename Graph<T>::entity cur : G.adj(num)) {
+		if (visited.insert(cur.to).second)
+			dfs(G, cur.to, visited, out);
+	}
+}
 #endif // !_GRAPH_HEAD_
 
 
